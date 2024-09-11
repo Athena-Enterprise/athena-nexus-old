597f3339b83d8cfc9cb49d2837da5572e4e904a3
ItzDusty <44943671+ItzDusty@users.noreply.github.com>
2024-09-11 13:21:51 +0200

    Updated name-spaces

--- discord-frontend/src/router/index.js
+++ discord-frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginScreen from '../views/LoginScreen.vue';
-import Dashboard from '../views/Dashboard.vue';
+import UserDashboard from '../views/UserDashboard.vue';
 import CreateBot from '../views/CreateBot.vue';
 import AdminDashboard from '../views/AdminDashboard.vue';
 
 const routes = [
   { path: '/', name: 'login', component: LoginScreen },
-  { path: '/dashboard', name: 'dashboard', component: Dashboard },
+  { path: '/dashboard', name: 'user-dashboard', component: UserDashboard },
   { path: '/create-bot', name: 'create-bot', component: CreateBot },
   { path: '/admin-dashboard', name: 'admin-dashboard', component: AdminDashboard },
 ];
