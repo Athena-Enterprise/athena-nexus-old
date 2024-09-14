6c15bd066ebda7f0b50638b5936ff774b3b269ef
ItzDusty <44943671+ItzDusty@users.noreply.github.com>
2024-09-14 19:16:54 +0200

    Updating vue config

--- discord-frontend/vue.config.js
+++ discord-frontend/vue.config.js
@@ -3,10 +3,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: [
-      'nexus.athenanetwork.gg'
-    ],
+    allowedHosts: ['nexus.athenanetwork.gg'],
     host: '0.0.0.0', // This allows external access
-    port: 8080, // You can change this if necessary
-  }
+    port: 8080, // Development server port
+    proxy: {
+      '/dev': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: { '^/dev': '/' }, // Rewrite /dev/ path
+        secure: false,
+      },
+    },
+  },
 })
